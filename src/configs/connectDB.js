7ff69d98472430d0113f66c3817b8c45f1524700
DinhQuyen <tuthanbodem@gmail.com>
2023-05-18 11:51:52 +0700

    code

---
+++ src/configs/connectDB.js
@@ -0,0 +1,10 @@
+// get the client
+import mysql from 'mysql2';
+// create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'nodejsbasic'
+});
+
+export default connection;
---
+++ src/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import configViewEngine from "./configs/viewEngine";
+import initWebRoute from "./route/web";
+
+require('dotenv').config();
+
+const app = express()
+const port = process.env.PORT || 8080;
+
+configViewEngine(app);
+initWebRoute(app);
+
+app.listen(port, () => {
+    console.log('Example app listenning at http://localhost:${port} ');
+})
